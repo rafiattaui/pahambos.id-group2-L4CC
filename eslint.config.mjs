a8b5df84113b198e86f092f1b5a7416119e09eb6
rafiattaui <athallah.rafi13@gmail.com>
2026-02-25 21:38:33 +0700

    setup eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    rules: {
+      "prefer-const": "error",
+      "no-unused-vars": "error",
+      "no-console": "warn",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-floating-promises": "error"
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:

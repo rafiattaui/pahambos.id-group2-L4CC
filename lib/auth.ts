3788fbb127956d512747dd254c1313f5ba05f914
rafiattaui <athallah.rafi13@gmail.com>
2026-02-26 20:41:16 +0700

    integrated betterauth.

---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    advanced: {
+        database: {
+            generateId: "uuid",
+        }
+    },
+    emailAndPassword: {
+        enabled: true
+    },
+    // TODO - include google authentication
+});

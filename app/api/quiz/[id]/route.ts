01e53a7854c6f84ba46421797f2d87cc33d30bd4
rafiattaui <athallah.rafi13@gmail.com>
2026-03-04 15:21:17 +0700

    finished route for retrieving a list of quizzes.

--- app/api/quiz/[id]/route.ts
+++ app/api/quiz/[id]/route.ts
@@ -3,6 +3,9 @@ import { handleError } from '@/lib/api/errors';
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+/*
+ * Retrieve a quiz and its contents via its UUID.
+ */
 export const GET = WithAuth(async (req, { user, params }) => {
   try {
     const { id } = await params;

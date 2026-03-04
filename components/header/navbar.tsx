3f7ea7a6257323126b3d48298ce3126e71b7a7ee
talahanakrajin <athallahraja8@gmail.com>
2026-03-04 11:43:54 +0700

    responsive page (unfinished)

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -13,33 +13,39 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="container max-w-full flex flex-row items-center">
+    <nav className="container flex max-w-full flex-row items-center">
       <Logo></Logo>
-        <div className="hidden md:flex flex-1 items-center gap-4">
+      <div className="hidden flex-1 items-center gap-4 md:flex">
+        <Button variant="ghost" onClick={handleClick}>
+          Discover
+        </Button>
+        <Button variant="ghost" onClick={handleClick}>
+          Learn
+        </Button>
+        <div className="flex flex-row">
           <Button variant="ghost" onClick={handleClick}>
-            Discover
+            Create <Plus />
           </Button>
-          <Button variant="ghost" onClick={handleClick}>
-            Learn
-          </Button>
-          <div className="flex flex-row">
-            <Button variant="ghost" onClick={handleClick}>
-              Create <Plus />
-            </Button>
         </div>
-          
-        </div>
-        <div className="flex items-center gap-2 ml-auto">
-          <Button onClick={handleClick} className="bg-blue-500 text-white hover:bg-blue-700">
-            Sign Up
+      </div>
+      <div className="ml-auto flex items-center gap-2">
+        <a href="/register">
+          <Button
+            onClick={handleClick}
+            className="bg-blue-500 text-white hover:bg-blue-700"
+          >
+            Register
           </Button>
+        </a>
+        <a href="/login">
           <Button variant="ghost" onClick={handleClick}>
             Log In
           </Button>
-          <div className="flex h-10 w-10 items-center justify-center rounded hover:bg-gray-200 mr-4">
-            <CircleUserRound className="h-max w-max" />
-          </div>
+        </a>
+        <div className="mr-4 flex h-10 w-10 items-center justify-center rounded hover:bg-gray-200">
+          <CircleUserRound className="h-max w-max" />
         </div>
+      </div>
     </nav>
   );
 }

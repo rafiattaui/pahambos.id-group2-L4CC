2aac9fec9366e55d9aef47f06bb69cbf8ad7d89f
rafiattaui <athallah.rafi13@gmail.com>
2026-03-05 12:39:52 +0700

    fixed any types in jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,19 +1,21 @@
-import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "util";
-import React from "react";
+import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+import React from 'react';
 
-// Polyfill for TextEncoder/Decoder (needed for some MSW or Jose-based auth setups)
-if (typeof globalThis.TextEncoder === "undefined") {
-  (globalThis as any).TextEncoder = TextEncoder;
+interface GlobalWithText {
+  TextEncoder: typeof TextEncoder;
+  TextDecoder: typeof TextDecoder;
 }
 
-if (typeof globalThis.TextDecoder === "undefined") {
-  (globalThis as any).TextDecoder = TextDecoder;
+if (typeof globalThis.TextEncoder === 'undefined') {
+  (globalThis as unknown as GlobalWithText).TextEncoder = TextEncoder;
 }
 
 // Global Mock for Lucide Icons
 // This prevents Jest from trying to parse SVG components during tests
 jest.mock('lucide-react', () => ({
-  CircleUserRound: (props: any) => React.createElement('div', { ...props, 'data-testid': 'user-icon' }),
-  Plus: (props: any) => React.createElement('div', { ...props, 'data-testid': 'plus-icon' }),
+  CircleUserRound: (props: Record<string, unknown>) =>
+    React.createElement('div', { ...props, 'data-testid': 'user-icon' }),
+  Plus: (props: Record<string, unknown>) =>
+    React.createElement('div', { ...props, 'data-testid': 'plus-icon' }),
 }));

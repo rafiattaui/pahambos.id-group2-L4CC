c4e435713a7bfcdfb077494a5b331f7073aa6c7c
talahanakrajin <athallahraja8@gmail.com>
2026-03-29 22:03:22 +0700

    placeholder

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,5 @@
+import DashboardMain from '@/components/dashboardComp/dashboardMain';
+
 export default function Dashboard() {
-  return (
-    <div className="p-8">
-      <h1 className="mb-4 text-3xl font-bold">Dashboard</h1>
-      <p>
-        Welcome to your dashboard! Here you can manage your projects and
-        settings.
-      </p>
-    </div>
-  );
+  return <DashboardMain />;
 }

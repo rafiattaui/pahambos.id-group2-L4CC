6b93a2305c32f84af3ff62955bc847f8c23a2679
salmon11062006 <csalomo7@gmail.com>
2026-04-01 13:35:30 +0700

    updated pages

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -11,7 +11,7 @@ export default async function SignupPage() {
       <div className="grid flex-1 lg:grid-cols-2">
         <div className="flex flex-col gap-4 p-6 md:p-10">
           <div className="flex flex-1 items-center justify-center">
-            <div className="w-full max-w-xs">
+            <div className="w-full max-w-xs rounded-2xl bg-white p-6 shadow-lg">
               <SignupForm />
             </div>
           </div>

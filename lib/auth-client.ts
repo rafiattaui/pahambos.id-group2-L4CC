be849f2979ccc88e8ae41d3e7d53830425551c18
rafiattaui <athallah.rafi13@gmail.com>
2026-04-08 17:24:51 +0700

    middleware function and isAuthenticated function for checking.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,12 @@
 import { createAuthClient } from 'better-auth/react';
+import { NextRequest } from 'next/server';
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
 });
+
+export async function isAuthenticated(request: NextRequest) {
+  const data = await authClient.getSession();
+
+  return data === null;
+}

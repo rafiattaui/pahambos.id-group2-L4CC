b38dc47dce831773c0f895e32197c9c4ff513ed6
rafiattaui <athallah.rafi13@gmail.com>
2026-04-08 17:24:51 +0700

    middleware function and isAuthenticated function for checking.

---
+++ app/proxy.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { isAuthenticated } from '@/lib/auth-client';
+
+export async function proxy(request: NextRequest) {}

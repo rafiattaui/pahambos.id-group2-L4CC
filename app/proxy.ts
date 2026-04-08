3eb3a9b415273398bfe328d53373f2e6bb8f79bc
rafiattaui <athallah.rafi13@gmail.com>
2026-04-08 18:16:44 +0700

    empty proxy.

--- app/proxy.ts
+++ app/proxy.ts
@@ -1,5 +1,3 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { isAuthenticated } from '@/lib/auth-client';
 
 export async function proxy(request: NextRequest) {}

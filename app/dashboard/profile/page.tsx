89c9e71da3b364b66502ebbff117727a272a57f5
Christian Salomo <csalomo7@gmail.com>
2026-05-11 18:28:55 +0700

    connecting profile card to database

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,9 +1,18 @@
 import ProfileCard from '@/components/profileCard';
+import { auth } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 
-export default function Profile() {
+export default async function Profile() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) redirect('/login');
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <ProfileCard />
+      <ProfileCard user={session.user} />
     </div>
   );
 }

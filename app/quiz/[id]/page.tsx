dc7283c05093ebb1aca7947829563e8ed03f252d
rafiattaui <athallah.rafi13@gmail.com>
2026-05-20 11:29:40 +0700

    fixed quiz retrieval based on uuid.

--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -2,15 +2,19 @@ import { prisma } from '@/lib/prisma';
 import { notFound } from 'next/navigation';
 import QuizInterface from '@/components/question-types/QuizInterface';
 
-export default async function QuizPage({
-  params,
-}: {
-  params: { quizId: string };
-}) {
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function QuizPage({ params }: Props) {
+  const { id } = await params;
+  console.log('Quiz ID:', id);
   const questions = await prisma.quizQuestion.findMany({
-    where: { quizId: params.quizId },
+    where: { quizId: id },
     orderBy: { order: 'asc' },
   });
+
+  console.log('Fetched Questions:', questions); // Debug log to check fetched questions
 
   if (!questions.length) return notFound();
 

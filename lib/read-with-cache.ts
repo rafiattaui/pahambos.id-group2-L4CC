d6748641fc2f4c31d5ca6cf6924325b842d0f582
rafiattaui <athallah.rafi13@gmail.com>
2026-06-08 19:47:43 +0700

    add: quizquestion cache invalidation based on updates to the quiz or the quizquestion itself.

--- lib/read-with-cache.ts
+++ lib/read-with-cache.ts
@@ -28,10 +28,11 @@ export async function GetQuestionWithCache(
   return PublicQuestionSchema.parse(question);
 }
 
-export async function invalidateQuestionCache(
-  quizId: string,
-  questionIndex: number
-) {
-  const cacheKey = `quiz:${quizId}:question:${questionIndex}`;
-  await redis.del(cacheKey);
+export async function invalidateQuestionCache(quizId: string) {
+  // because orders could be out of sync,
+  // we should just invalidate all questions for the quiz to be safe
+  const keys = await redis.keys(`quiz:${quizId}:question:*`);
+  if (keys.length > 0) {
+    await redis.del(keys);
+  }
 }

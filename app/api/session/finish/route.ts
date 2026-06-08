b8abbb871fb308091b09f689325c390ed308a07b
rafiattaui <athallah.rafi13@gmail.com>
2026-06-08 18:54:13 +0700

    add: integrated feedback logic within a function, to be called inside of /session/finish.

--- app/api/session/finish/route.ts
+++ app/api/session/finish/route.ts
@@ -6,6 +6,10 @@ import redis from '@/lib/redis';
 import { r_MetricsSchema } from '@/lib/schemas/sessionschemas';
 import { prisma } from '@/lib/prisma';
 import { SCORE_PER_QUESTION } from '../question/route';
+import { model } from '@/lib/groq';
+import { generateText, Output } from 'ai';
+import { type GroqLanguageModelOptions } from '@ai-sdk/groq';
+import { z } from 'zod';
 
 export const POST = WithAuth(async (req, { user, params }) => {
   try {
@@ -58,8 +62,13 @@ export const POST = WithAuth(async (req, { user, params }) => {
     pipe.del(`session:${session.id}`);
     pipe.del(`metrics:${session.id}`);
     pipe.del(`player_session:${user.id}`);
-    pipe.del(`session:${session.id}:answers`);
+    // pipe.del(`session:${session.id}:answers`); don't delete this yet
+    // we need to generate feedback from ai so we need the answers for that.
     await pipe.exec();
+
+    // ai feedback logic
+
+    const answers = await redis.lrange(`session:${session.id}:answers`, 0, -1);
 
     return NextResponse.json({
       success: true,

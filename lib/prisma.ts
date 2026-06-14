1dd9888f8c7fdc943a699f001ed90b3c3f5c0636
rafiattaui <athallah.rafi13@gmail.com>
2026-06-14 18:33:28 +0700

    pinecone setup.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,21 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
+import { Pinecone } from '@pinecone-database/pinecone';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPinecone = global as unknown as { pinecone: string };
 
 const connectionString = `${process.env.DATABASE_URL}`;
+const pinecone = `${process.env.PINECONE_API_KEY}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
-export { prisma };
+const pc = new Pinecone({
+  apiKey: pinecone,
+});
+
+const quizIndex = `pahambos-quiz`;
+const questionIndex = `pahambos-quizquestion`;
+
+export { prisma, pc, quizIndex, questionIndex };
